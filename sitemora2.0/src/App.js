324e7403861ca3887352452cbc5cf4f952eaf5de
moraelhumano <moraelhumano@gmail.com>
2021-02-19 23:43:50 -0600

    first commit

---
+++ sitemora2.0/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "../src/components/navbar"
+import './App.css';
+
+function App() {
+  return (
+    <Navbar></Navbar>
+  );
+}
+
+export default App;
---
+++ sitemora2.0/src/components/navbar.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Navbar = () => (
+  <header className="">Hola mundo</header>
+)
+
+export default Navbar
